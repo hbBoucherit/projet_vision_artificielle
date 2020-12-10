99e72f4c8afe0a1c664ea75f8be58882650b9fe5
hbBoucherit <hboucheri001@ensc.fr>
2020-12-10 16:58:57 +0100

    VGG16 CNN + Working on flickr8k

--- va_project.py
+++ va_project.py
@@ -1,32 +1,59 @@
-# IMPORTS 
+import sys, time, os, warnings 
+
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd 
 
-import tensorflow.keras
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPool2D, BatchNormalization
-import tensorflow.keras.optimizers as optimizers
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from collections import OrderedDict
 
-import sys, os
-sys.path.append(os.path.join('', "coco/"))
-from mrcnn import utils
-import mrcnn.model as modellib
-import coco
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.applications import VGG16
 
-class InferenceConfig(coco.CocoConfig):
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
+#import keras
+#from keras.backend.tensorflow_backend import set_session
+#from collections import Counter 
 
-config = InferenceConfig()
+# Location of the Flickr8k images
+dataset_image_path ="./flickr8k/Images/"
+# Location of the caption file
+dataset_text_path  ="./flickr8k/captions.txt" 
+# Wanted shape for images
+wanted_shape = (224,224,3)
 
-#COCO MODEL
-model_weights = 'mask_rcnn_balloon.h5'
+# Using pretrained model VGG16
+feature_extractor = VGG16(
+    include_top=False, weights='imagenet', input_tensor=None,
+    input_shape=None, pooling=None, classes=1000,
+    classifier_activation='softmax'
+)
+# To freeze VGG16 weights
+feature_extractor.trainable=False
+feature_extractor.summary()
 
-# RCNN
-rcnn_model = modellib.MaskRCNN(mode='inference', model_dir='./', config=config)
-rcnn_model.load_weights('mask_rcnn_balloon.h5')
-rcnn_model.summary()
+'''
+final_model = Sequential([
+    feature_extractor
+])
+final_model.summary()
+'''
+# To obtain the text dataset corresponding to images
+df_texts = pd.read_csv(dataset_text_path) #["image","caption"] 
+n_img = df_texts.count()/5 # 40455/5 
 
-# CNN
+# To filter image column in df_texts so that images appear only one time
+unique_img = pd.unique(df_texts["image"])# 8091 unique images
 
+# Images saved to df_images as (224,224,3) for each unique img in the dataset
+df_images=np.array([np.array(load_img(dataset_image_path+unique_img[i], target_size=wanted_shape)) for i in range(len(n_img))])
 
+# To obtain the feature maps
+images = os.listdir(dataset_image_path)
+for i, name in enumerate(images):
+    filename = dataset_image_path + '/' + name
+    image = load_img(filename, target_size=(224,224,3))
+    image = img_to_array(image)
+    image = preprocess_input(image)
+    
