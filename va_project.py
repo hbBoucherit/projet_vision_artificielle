9bcce702ce14262b198db2e59685fa32149714bc
hbBoucherit <hboucheri001@ensc.fr>
2020-12-10 12:55:05 +0100

    RCNN

--- va_project.py
+++ va_project.py
@@ -1,21 +1,31 @@
 # IMPORTS 
 import numpy as np
-# Neural networks dependancies
+
 import tensorflow.keras
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPool2D, BatchNormalization
 import tensorflow.keras.optimizers as optimizers
 
-# DATA
-#MSCOCO
+import sys, os
+sys.path.append(os.path.join('', "coco/"))
+from mrcnn import utils
+import mrcnn.model as modellib
+import coco
 
+class InferenceConfig(coco.CocoConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+
+#COCO MODEL
+model_weights = 'mask_rcnn_balloon.h5'
 
 # RCNN
-
-rcnn = MaskRCNN(mode='inference', model_dir='./', config=(NAME="test", GPU_COUNT = 1, IMAGES_PER8GPU = 1, NUM_CLASSES=81))
-model = load_weight('mask_rcnn_balloon.h5', by_name=True)
-model.summary()
+rcnn_model = modellib.MaskRCNN(mode='inference', model_dir='./', config=config)
+rcnn_model.load_weights('mask_rcnn_balloon.h5')
+rcnn_model.summary()
 
 # CNN
 
