3c65eadb884691119f0736d10a1f826b2112cd0c
hbBoucherit <hboucheri001@ensc.fr>
2020-12-12 18:05:52 +0100

    Feature extractions

--- va_project.py
+++ va_project.py
@@ -7,6 +7,7 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.applications.vgg16 import preprocess_input
 from collections import OrderedDict
 
 from tensorflow.keras.models import Sequential
@@ -23,22 +24,6 @@ dataset_text_path  ="./flickr8k/captions.txt"
 # Wanted shape for images
 wanted_shape = (224,224,3)
 
-# Using pretrained model VGG16
-feature_extractor = VGG16(
-    include_top=False, weights='imagenet', input_tensor=None,
-    input_shape=None, pooling=None, classes=1000,
-    classifier_activation='softmax'
-)
-# To freeze VGG16 weights
-feature_extractor.trainable=False
-feature_extractor.summary()
-
-'''
-final_model = Sequential([
-    feature_extractor
-])
-final_model.summary()
-'''
 # To obtain the text dataset corresponding to images
 df_texts = pd.read_csv(dataset_text_path) #["image","caption"] 
 n_img = df_texts.count()/5 # 40455/5 
@@ -46,14 +31,74 @@ n_img = df_texts.count()/5 # 40455/5
 # To filter image column in df_texts so that images appear only one time
 unique_img = pd.unique(df_texts["image"])# 8091 unique images
 
-# Images saved to df_images as (224,224,3) for each unique img in the dataset
-df_images=np.array([np.array(load_img(dataset_image_path+unique_img[i], target_size=wanted_shape)) for i in range(len(n_img))])
+def padding(array, xx, yy):
+    """
+    :param array: numpy array
+    :param xx: desired height
+    :param yy: desirex width
+    :return: padded array
+    """
 
+    h = array.shape[0]
+    w = array.shape[1]
+
+    a = (xx - h) // 2
+    aa = xx - a - h
+
+    b = (yy - w) // 2
+    bb = yy - b - w
+
+    return np.pad(array, pad_width=((a, aa), (b, bb)), mode='constant')
+
+#print(padding(arr, 99, 13).shape)  => (99,13)
+
+# Function to crop images
+def crop_center(img):
+    cropx, cropy, _ = wanted_shape
+    y,x,_ = img.shape
+    startx = x//2-(cropx//2)
+    starty = y//2-(cropy//2)    
+    return img[starty:starty+cropy,startx:startx+cropx]
+
+# Images saved to df_images as (224,224,3) for each unique img in the dataset
+def load_img_from_ds(image_name):
+    #PREPROCESSING
+    img =  img_to_array(load_img(dataset_image_path+image_name, target_size=wanted_shape))
+    #img = crop_center(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    #print(f"shape img input {image_name} : {img.shape}")
+    return img
+
+# Using pretrained model VGG16
+base_model = VGG16(
+    include_top=True, weights='imagenet', input_tensor=None,
+    input_shape=wanted_shape, pooling=None, classes=1000,
+    classifier_activation='softmax'
+)
+# To freeze VGG16 weights
+#feature_extractor.trainable=False
+
+from tensorflow.keras.models import Model
+model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc2').output)
+model.summary()
+fm=[]
+for i in range(len(unique_img)):
+    img = load_img_from_ds(unique_img[i])
+    feature_map = model.predict(img)
+    print(feature_map.shape)
+    fm.append(feature_map)
+
+fm=np.array(fm)
+print(fm.shape)
+
+#print(f"Shape des fm {feature_maps.shape}")
 # To obtain the feature maps
+feature_maps = OrderedDict()
 images = os.listdir(dataset_image_path)
 for i, name in enumerate(images):
-    filename = dataset_image_path + '/' + name
-    image = load_img(filename, target_size=(224,224,3))
-    image = img_to_array(image)
-    image = preprocess_input(image)
-    
+    filename = dataset_image_path + name
+    image = load_img(filename, target_size=wanted_shape)
+    #prediction = 
+    #feature_maps[name] = 
+
